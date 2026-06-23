de028a5ba3189f4622548ff46c8b701ec9451731
Louis Greiner <louis.greiner@proton.me>
2026-06-23 16:32:35 +0200

    germany: add spatial
    Signed-off-by: Louis Greiner <louis.greiner@proton.me>

---
+++ germany.py
@@ -0,0 +1,64 @@
+import dotenv
+import mobility
+
+dotenv.load_dotenv()
+
+mobility.set_params(package_data_folder_path="./data", project_data_folder_path="./data")
+
+# de-083155012074 = Müllheim im Markgräflerland
+transport_zones = mobility.TransportZones("de-083155012074", radius=10.0)
+print(transport_zones.countries)
+
+# # TODO
+# survey = mobility.EMPMobilitySurvey()
+
+# # TODO
+# # Create a synthetic population of 1000 people for the area.
+# population = mobility.Population(transport_zones, sample_size=1000)
+
+# # TODO
+# # Simulate trips for this population with car, walk, and bicycle.
+# population_trips = mobility.PopulationGroupDayTrips(
+#     population=population,
+#     modes=[
+#         mobility.CarMode(transport_zones),
+#         mobility.WalkMode(transport_zones),
+#         mobility.BicycleMode(transport_zones),
+#     ],
+#     activities=[
+#         mobility.OtherActivity(population=population),
+#     ],
+#     surveys=[survey],
+#     parameters=mobility.GroupDayTripsParameters(
+#         run=mobility.GroupDayTripsRunParameters(n_iterations=1),
+#         mode_sequences=mobility.GroupDayTripsModeSequenceParameters(
+#             mode_sequence_search_parallel=False,
+#         ),
+#     ),
+# )
+
+# # TODO
+# # Run the weekday model.
+# weekday_run = population_trips.run("weekday")
+# weekday_plan_steps = weekday_run.get()["plan_steps"].collect()
+
+# # TODO
+# # Use population_trips.results(...) as the main entry point for indicators.
+# weekday_results = population_trips.results("weekday")
+# trip_count_by_mode = weekday_results.metrics.trip_count(
+#     by_variable="mode",
+#     iterations="last",
+#     output="table",
+# )
+
+# # TODO
+# # Plot origin-destination flows between transport zones.
+# od_flow_plot = weekday_results.metrics.trip_count(
+#     by_zone=["origin_zone", "destination_zone"],
+#     iterations="last",
+#     output="plot",
+# )
+
+# # TODO
+# # Get a report of the parameters used by the model.
+# parameters_report = weekday_run.parameters_dataframe()
--- mobility/spatial/countries.py
+++ mobility/spatial/countries.py
@@ -1,19 +1,22 @@
-from mobility.spatial.admin_units import FrenchAdminUnits, SwissAdminUnits
+from mobility.spatial.admin_units import FrenchAdminUnits, GermanAdminUnits, SwissAdminUnits
 from mobility.spatial.france import FrenchLocalAdminUnitsCategories
+from mobility.spatial.germany import GermanLocalAdminUnitsCategories
 from mobility.spatial.switzerland import SwissLocalAdminUnitsCategories
 
 
 def available_admin_units():
     """Return built-in admin-unit data by country."""
     return {
-        "fr": (FrenchAdminUnits, "commune"),
-        "ch": (SwissAdminUnits, "municipality"),
+        # "fr": (FrenchAdminUnits, "commune"),
+        # "ch": (SwissAdminUnits, "municipality"),
+        "de": (GermanAdminUnits, "gemeinde"),
     }
 
 
 def available_local_admin_unit_categories():
     """Return built-in local-admin-unit category data by country."""
     return {
-        "fr": FrenchLocalAdminUnitsCategories(),
-        "ch": SwissLocalAdminUnitsCategories(),
+        # "fr": FrenchLocalAdminUnitsCategories(),
+        # "ch": SwissLocalAdminUnitsCategories(),
+        "de": GermanLocalAdminUnitsCategories(),
     }
