f0bc711b7fa83314fb8929f076ae98fbe37a93e8
Louis Greiner <louis.greiner@proton.me>
2026-06-23 18:14:49 +0200

    new population_trips
    Signed-off-by: Louis Greiner <louis.greiner@proton.me>

--- germany.py
+++ germany.py
@@ -14,33 +14,60 @@ mobility.set_params(package_data_folder_path="./data", project_data_folder_path=
 transport_zones = mobility.TransportZones("de-083155012074", radius=10.0)
 print(transport_zones.countries)
 
-# # TODO
-# survey = mobility.EMPMobilitySurvey()
+# TODO
+survey = mobility.EMPMobilitySurvey()
 
 # # TODO
 # # Create a synthetic population of 1000 people for the area.
-# population = mobility.Population(transport_zones, sample_size=1000)
+population = mobility.Population(transport_zones, sample_size=1000)
 
-# # TODO
-# # Simulate trips for this population with car, walk, and bicycle.
-# population_trips = mobility.PopulationGroupDayTrips(
-#     population=population,
-#     modes=[
-#         mobility.CarMode(transport_zones),
-#         mobility.WalkMode(transport_zones),
-#         mobility.BicycleMode(transport_zones),
-#     ],
-#     activities=[
-#         mobility.OtherActivity(population=population),
-#     ],
-#     surveys=[survey],
-#     parameters=mobility.GroupDayTripsParameters(
-#         run=mobility.GroupDayTripsRunParameters(n_iterations=1),
-#         mode_sequences=mobility.GroupDayTripsModeSequenceParameters(
-#             mode_sequence_search_parallel=False,
-#         ),
-#     ),
-# )
+# TODO
+# Simulate trips for this population with car, walk, and bicycle.
+population_trips = mobility.PopulationGroupDayTrips(
+    population=population,
+    modes=[
+        mobility.CarMode(transport_zones),
+        mobility.WalkMode(transport_zones),
+        mobility.BicycleMode(transport_zones),
+    ],
+    activities=[
+        mobility.OtherActivity(population=population),
+        mobility.HomeActivity(),
+        mobility.WorkActivity(),
+    ],
+    surveys=[survey],
+    parameters=mobility.GroupDayTripsParameters(
+        run=mobility.GroupDayTripsRunParameters(n_iterations=5),
+    ),
+    activity_sequences=GroupDayTripsActivitySequenceParameters(
+        k_activity_sequences=3,
+    ),
+    destination_sequences=GroupDayTripsDestinationSequenceParameters(
+        k_destination_sequences=3,
+        alpha=0.25,
+        dest_prob_cutoff=0.9,
+        cost_uncertainty_sd=1.0,
+        refresh_active_mode_alternatives=True,  
+    ),
+    mode_sequences=GroupDayTripsModeSequenceParameters(
+        k_mode_sequences=3,
+        mode_sequence_search_parallel=True,
+        use_rust_mode_sequence_search=True,
+    ),
+    plan_update=GroupDayTripsPlanUpdateParameters(
+        transition_logit_scale=0.25,
+        update_plan_timings_from_modeled_travel_times=True,
+        transition_revision_probability=0.4,
+        n_warmup_iterations=0,
+        max_inactive_age=1,
+        transition_utility_pruning_delta=1.0,
+        plan_probability_pruning_retained_share=0.95,
+        plan_probability_pruning_min_iteration=2,
+        min_transition_utility_gain=0.1,
+        use_destination_shadow_prices=True,
+        min_activity_time_constant=2.0,
+    ),
+)
 
 # # TODO
 # # Run the weekday model.
